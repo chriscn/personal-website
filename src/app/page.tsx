da0cf31f5f1b58b282251fa3a7c6bcbd9e225f50
Christopher Nethercott <ccnethercott@gmail.com>
2023-12-18 13:44:29 +0000

    feat: update project section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
 import Image from "next/image";
+import Link from "next/link";
+
+type Project = {
+	name: string;
+	description: string;
+	url: string;
+};
+
+const projects: Project[] = [
+	{
+		name: "University of York's Week Numbers",
+		description:
+			"iCalendar (RFC 5545) format of the University of York's week numbers; now with semester support!",
+		url: "https://github.com/chriscn/uoy-week-numbers",
+	},
+	{
+		name: "Dispixel",
+		description:
+			"A Discord Hypixel Bot to query a player's statistics using the Hypixel API",
+		url: "https://github.com/chriscn/Dispixel",
+	},
+];
 
 export default function IndexPage() {
 	return (
 		<main className="h-screen flex flex-col items-center justify-center bg-slate-50 font-serif">
-			<section className="m-4 space-y-4 max-w-xl">
+			<section className="m-4 space-y-4 max-w-screen-sm w-full p-4">
 				<div className="flex flex-row space-x-4 items-center text-slate-800">
 					<Image
 						src="/profile.png"
@@ -12,28 +34,27 @@ export default function IndexPage() {
 						className="rounded-xl"
 						alt="Christopher Nethercott profile picture"
 					/>
-					<div className="w-full">
-						<h1 className="text-2xl font-medium">Christopher Nethercott</h1>
+					<div>
+						<h1 className="text-2xl font-bold">Christopher Nethercott</h1>
 						<p>Infrastructure stuff at a University</p>
-						<p>
-							<b>Education: </b> Univeristy of York Mathematics
-						</p>
 					</div>
 				</div>
 				<div className="flex flex-col sm:flex-row justify-between">
 					<div className="w-full">
-						<h1 className="text-xl">contact me:</h1>
-						<p>email</p>
-						<p>mastodon</p>
-						<p>linkedin</p>
-						<p>github</p>
-					</div>
-					<div className="w-full">
 						<h1 className="text-xl">projects:</h1>
-						<p>Univeristy of York's Week Numbers</p>
-						<p>mastodon</p>
-						<p>linkedin</p>
-						<p>github</p>
+						<ul>
+							{projects.map((project) => (
+								<li key={project.name}>
+									<Link
+										href={project.url}
+										className="hover:underline text-blue-500"
+									>
+										{project.name}
+									</Link>
+									<p className="text-balance">{project.description}</p>
+								</li>
+							))}
+						</ul>
 					</div>
 				</div>
 			</section>
